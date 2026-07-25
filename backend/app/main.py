af434b07b6c016dfbf0a31244fdd3e5a5506020e
Gotam Dulhani <ghotamdulhani123@gmail.com>
2026-07-25 23:52:32 +0500

    fix: allow all CORS origins to fix frontend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 origins = [origin.strip() for origin in settings.BACKEND_CORS_ORIGINS.split(",")]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
